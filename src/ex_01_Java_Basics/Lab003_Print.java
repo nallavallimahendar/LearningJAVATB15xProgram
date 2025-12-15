c9fefa3da179a0e417421d05e8eba2e3a045a643
mahendar <nallavallimahendar@gmail.com>
2025-12-15 15:40:32 +0530

    Second Message

---
+++ src/ex_01_Java_Basics/Lab003_Print.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab003_Print {
+    static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}
