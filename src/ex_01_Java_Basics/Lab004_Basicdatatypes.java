fcb88088af2cd0726b7cda60481cd83a5fe85282
mahendar <nallavallimahendar@gmail.com>
2025-12-17 22:21:42 +0530

    Varaibles and 9table

---
+++ src/ex_01_Java_Basics/Lab004_Basicdatatypes.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+public class Lab004_Basicdatatypes {
+    static void main(String[] args) {
+        //Primitive data types
+        byte b=10;
+        short s=129;
+        int i=200;
+        char c='a';
+        float f=0.12f;
+        long l=1234678L;
+        double d=0.12345678;
+    }
+}
---
+++ src/ex_01_Java_Basics/Lab005_Java9table.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basics;
+
+public class Lab005_Java9table {
+    static void main(String[] args) {
+        int t=9;
+        System.out.printf("%d*1=%d",t,t);
+        System.out.printf("%d*2=%d",t,t*2);
+        System.out.printf("%d*3=%d",t,t*3);
+        System.out.printf("%d*4=%d",t,t*4);
+        System.out.printf("%d*5=%d",t,t*5);
+        System.out.printf("%d*6=%d",t,t*6);
+        System.out.printf("%d*7=%d",t,t*7);
+        System.out.printf("%d*8=%d",t,t*8);
+        System.out.printf("%d*9=%d",t,t*9);
+        System.out.printf("%d*10=%d",t,t*10);
+    }
+}
